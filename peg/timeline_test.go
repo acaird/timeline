89a02fcbba80487fad55efcd4dac879787ca925a
Andy Caird <acaird@gmail.com>
2025-11-21 17:31:38 -0500

    very early efforts at a PEG for these charts
    based on the Pigeon parser generator
      - https://pkg.go.dev/github.com/mna/pigeon
      - https://github.com/mna/pigeon
      - https://prasanthmj.github.io/go/peg-parser-in-go/

---
+++ peg/timeline_test.go
@@ -0,0 +1,16 @@
+package timeline
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	filename := "the_cure.data"
+	b, err := ParseFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", b)
+
+}
